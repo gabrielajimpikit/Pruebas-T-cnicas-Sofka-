31ca22e2d2be066584bdc41c3986dc7d0f2cefb1
Lida Gabriela <71735060+gabrielajimpikit@users.noreply.github.com>
2025-09-12 21:35:53 -0500

    Create CheckoutRunner.java

---
+++ serenity-opencart/src/test/java/starter/runners/CheckoutRunner.java
@@ -0,0 +1,14 @@
+package starter.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features/opencart",
+        glue = "starter.stepdefinitions"
+)
+public class CheckoutRunner {
+}
